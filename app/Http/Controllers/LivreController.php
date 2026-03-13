939bed4d286748e562d433101aea33cd2b0839b5
MariaE003 <elgotbymaria@gmail.com>
2026-03-13 14:43:46 +0000

    ajouter la methode save  dans LivreController pour creer un livre

---
+++ app/Http/Controllers/LivreController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Livre;
+use App\Models\Categorie;
+class LivreController extends Controller
+{
+    public function save(Request $request){
+        $request->validate([
+            'titre'=>'required',
+            'auteur'=>'required',
+            'quantite'=>'required',
+            'nbr_degrade'=>'required',
+            'categorie_id'=>'required',
+        ]);
+        $livre=Livre::create([
+            'titre'=>$request->titre,
+            'auteur'=>$request->auteur,
+            'quantite'=>$request->quantite,
+            'nbr_degrade'=>$request->nbr_degrade,
+            'categorie_id'=>$request->categorie_id,
+        ]);
+
+        return response()->json([
+            'message'=>'livre ajouter avec succes',
+            'categorie'=>$livre,
+        ]);
+
+    }
+    
+
+}
+
