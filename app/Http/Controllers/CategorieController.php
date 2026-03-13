3d56157a4dcc535c9aa07c370b97e66e2c7c9ff3
MariaE003 <elgotbymaria@gmail.com>
2026-03-13 14:37:55 +0000

    ajouter la methode store dans CategorieController pour creer une categorie

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+
+class CategorieController extends Controller
+{
+    //
+    public function store(Request $request){
+        $request->validate([
+            'name'=>'required',
+        ]);
+        $categorie=Category::create([
+            'name'=>$request->name,
+        ]);
+
+        return response()->json([
+            'message'=>'categorie ajouter avec succes',
+            'categorie'=>$categorie,
+        ],201);
+
+    }
+    
+}
