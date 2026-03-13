a0eba204b804a1c4053c362e55952989db3b8273
MariaE003 <elgotbymaria@gmail.com>
2026-03-13 19:58:08 +0000

    ajouter les routes necessaires

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,52 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\LivreController;
 
 
 
-
+// categorie
 Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/login',[AuthController::class,'login'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function (){
+    
+    // Route::post('/categorie',[CategorieController::class,'save']);//done
+    // Route::post('/categorie/show',[CategorieController::class,'show']);
+    
+    // Route::delete('/categorie/{id}/delete',[CategorieController::class,'delete']);
+    // Route::get('/categories/{category}',[CategorieController::class,'show']);
+
+    Route::apiResource('categories',CategorieController::class);
+    
+    // livre
+    Route::post('/livre',[LivreController::class,'save']);//done
+    Route::get('/livre/show',[LivreController::class,'show']);
+    // get
+    Route::delete('/livre/{id}/delete',[LivreController::class,'delete']);
+    
+    Route::put('/livre/{id}',[LivreController::class,'update']);
+    // search
+    Route::get('/livre/search',[LivreController::class,'search']);
+    
+    Route::get('/livre/LivresParCategorie',[LivreController::class,'LivresParCategorie']);
+    
+    Route::get('/livre/showOne/{id}',[LivreController::class,'showOne']);
+    
+    Route::get('/livre/populaire',[LivreController::class,'LivrePlusConsulter']);
+    
+    Route::get('/livre/livreDegrade',[LivreController::class,'livreDegrade']);
+    
+    Route::get('/livre/statistique',[LivreController::class,'statistique']);
+});
+
+
+
+
 
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::get('/profile',[AuthController::class,'profile']);
     Route::post('/logout',[AuthController::class,'logout']);
 });
 
